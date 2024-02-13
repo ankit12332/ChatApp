808f415079c459481a8ee29b7b6003cbf2901604
ankit12332 <ankit.panda010.ap@gmail.com>
2024-02-13 23:45:11 +0530

    Added User Entity Along with Configured ConnectionString for the database

---
+++ ChatApp/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using ChatApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ChatApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
